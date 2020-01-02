e3d131685764d486cf95e6d3f69e452d872b339d
keyf <kyf456@163.com>
2020-01-02 10:01:15 +0800

    debug

--- zip.sh
+++ zip.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 rm -v youxibd.zip
 mkdir youxibd
-cp -v ./youxibd.ico ./ui/static/
+cp -v ./youxibd.ico ./ui/favicon.ico
 cp -rv ./ui ./youxibd
 cp -rv ./certs ./youxibd
 cp -v ./steamcommunity-proxy.exe ./youxibd/游戏便当Steam社区加速.exe
